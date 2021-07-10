f9d4cc04f46706101d1e8949f6c2a17ba6d4ee11
gollapallisaikrishna <82506161+gollapallisaikrishna@users.noreply.github.com>
2021-07-10 10:30:44 +0530

    Game

--- Python.py
+++ Python.py
@@ -3,10 +3,32 @@ def sum_of(a,b):
     return sum(a,b)
   else:
     return 'There are no Numbers'
+ 
 
-  from random import shuffle 
-  mylist = ['','O','','']
-  def of myfunc(a):
-    mylist.shuffle()
-    retrun mylist
+mylist = ['','O','','']
+from random import shuffle 
+shuffle(mylist)
+
+
+def myfunc():
+    shuffle(mylist)
+    return mylist
     
+def user_guess():
+    guess = ''
+    while guess not in ['0','1','2']:
+        guess = input('Give a number from 0,1,2:')
+    return int(guess)
+
+def inter(mylist,guess):
+    if mylist[guess] == 'O':
+        print('correct')
+        print(mylist)
+    else:
+        print('Wrong')
+        print(mylist)
+    
+mylist = ['','O','']
+result = myfunc()
+key = guess()
+inter(result,key)
